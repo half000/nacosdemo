b395b43e7e150317c1399912c08af498f99d0473
wangwei <271936145@qq.com>
2020-04-08 10:49:08 +0800

    nacosconsumer

---
+++ nacosconsumer/src/main/java/com/half/nacos/web/PersonRestController.java
@@ -0,0 +1,58 @@
+package com.half.nacos.web;
+
+import com.half.nacos.model.Person;
+import org.springframework.web.bind.annotation.*;
+
+/**
+ * {@link } {@link RestController}
+ * 广告资源位...
+ *
+ * @author mercyblitz
+ * @date 2017-10-14
+ **/
+@RestController
+//支持跨域请求
+@CrossOrigin
+public class PersonRestController {
+
+    @GetMapping("/person/{id}")
+    public Person person(@PathVariable Long id,
+                         @RequestParam(required = false) String name){
+        Person person = new Person();
+        person.setId(id);
+        person.setName(name);
+        return person;
+    }
+
+    @PostMapping(value = "/person/json/to/properties",
+            consumes = "text/plain",
+            produces = "application/properties+person" // 响应类型
+    )
+    public Person personJsonToProperties(@RequestParam String json) {
+
+        // JSON to Map
+        // Map to Properties
+        return null;
+    }
+
+    @PostMapping(value = "/person/json/to/properties"
+           //, produces = "application/properties+person" // 响应类型
+           //, consumes =  MediaType.APPLICATION_JSON_UTF8_VALUE //请求类型
+    )
+    public Person personJsonToProperties(@RequestBody Person person) {
+        // @RequestBody 的内容是 JSON
+        // 响应的内容是 Properties
+        return person;
+    }
+
+    @PostMapping(value = "/person/properties/to/json"
+           //, produces =  MediaType.APPLICATION_JSON_UTF8_VALUE// 响应类型 // Accept
+          // , consumes = "application/properties+person"// 请求类型 // Content-Type
+    )
+    public Person personPropertiesToJson(@RequestBody Person person) {
+        // @RequestBody 的内容是 Properties
+        // 响应的内容是 JSON
+        return person;
+    }
+
+}
