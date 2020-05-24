d3c9bc4bbce6e375f3038c212b6f8159983b20d6
wangwei <271936145@qq.com>
2020-05-24 20:29:06 +0800

    去掉多余注解
    Signed-off-by: wangwei <271936145@qq.com>

--- nacosconsumer/src/main/java/com/half/nacos/NacosConsumer.java
+++ nacosconsumer/src/main/java/com/half/nacos/NacosConsumer.java
@@ -2,16 +2,12 @@ package com.half.nacos;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.cloud.client.SpringCloudApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 
 /**
  * @Author: wangwei
  * @Date: 2019/11/18 21:58
  */
 @SpringCloudApplication
-@EnableHystrix
-@EnableDiscoveryClient
 public class NacosConsumer {
 
     public static void main(String[] args) {
