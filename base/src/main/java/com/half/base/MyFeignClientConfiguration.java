0e11eeeede3052e1053062af3cd68573803862a3
wangwei <271936145@qq.com>
2020-11-18 01:28:46 +0800

    关于feign的一些注释 说明
    Signed-off-by: wangwei <271936145@qq.com>

--- base/src/main/java/com/half/base/MyFeignClientConfiguration.java
+++ base/src/main/java/com/half/base/MyFeignClientConfiguration.java
@@ -7,24 +7,46 @@ import com.netflix.hystrix.HystrixCommandKey;
 import com.netflix.hystrix.HystrixThreadPoolKey;
 import feign.Contract;
 import feign.Feign;
+import feign.ReflectiveFeign;
 import feign.Request;
 import feign.hystrix.SetterFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 /**
+ * feign 配置
+ * feign主要是把方法与url映射关联起来，通过对象方法调用来发出http请求
+ * <p>
+ * 关联方式是通过注解方式，现支持2种注解合约，一种是mvc的注解，一种是feign自带的注解
+ * <p>
+ * Feign客户端来实现对象方法调用的方式发出http请求，是采用动态代理来实现的，具体是 feign.Feign#newInstance(feign.Target)方法返回的是代理对象
+ *
  * @Author: wangwei
  * @Date: 2019-11-25 22:24
+ * @see ReflectiveFeign  通过基本实现类的newInstance创建代理对象，通过
+ * @see feign.InvocationHandlerFactory  来控制调用句柄的实例化,这里支持各种扩展
+ * @see ReflectiveFeign.ParseHandlersByName 创建方法handle映射（这里根据合约类型创建）
  */
 @Configuration
 public class MyFeignClientConfiguration {
-    /*
-    feign url映射策略
+    /**
+     * 支持2种注解合约，一种是mvc的注解，一种是feign自带的注解
+     * feign url映射策略
      */
     //@Bean
-    public Contract contract(){
+    public Contract contract() {
         return new Contract.Default();
     }
+
+    /**
+     * feign日志记录的设置
+     * Optional供给，可被动态配置覆盖
+     * @return
+     */
+    /*@Bean
+    Logger.Level feignLoggerLevel(){
+        return Logger.Level.FULL;
+    }*/
 
     /***
      *  自定义setter工厂，这里可以改变hystrix的配置
@@ -32,8 +54,8 @@ public class MyFeignClientConfiguration {
      * @return: feign.hystrix.SetterFactory
      */
     @Bean
-    public SetterFactory getSetterFactory(){
-        return (target,method)->{
+    public SetterFactory getSetterFactory() {
+        return (target, method) -> {
             String groupKey = target.name();
             String commandKey = Feign.configKey(target.type(), method);
             return HystrixCommand.Setter
--- base/src/main/java/com/half/base/ProviderFeignConfiguration.java
+++ base/src/main/java/com/half/base/ProviderFeignConfiguration.java
@@ -1,11 +1,27 @@
 package com.half.base;
 
+import feign.ReflectiveFeign;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.cloud.openfeign.FeignAutoConfiguration;
+import org.springframework.cloud.openfeign.FeignClientsConfiguration;
 import org.springframework.context.annotation.Configuration;
 
 /**
+ * 通过注册器导入 FeignClientFactoryBean 与 自定义FeignClientSpecification配置
+ *
  * @Author: wangwei
  * @Date: 2019-11-30 13:25
+ * @see org.springframework.cloud.openfeign.FeignClientsRegistrar
+ * @see org.springframework.cloud.openfeign.FeignClientFactoryBean#getObject()
+ * FeignClientFactoryBean创建feign客户端，首先获取FeignContext，然后通过FeignContext来创建Feign.Builder
+ * （也是个上下文，默认配置
+ * @see FeignClientsConfiguration ，和ribbon的套路一样），
+ * 然后builder创建feign
+ * @see ReflectiveFeign
+ * ，再feign.ReflectiveFeign#newInstance(feign.Target)创建对应的代理类
+ * <p>
+ * FeignContext由spring上下文管理，可定制化
+ * @see FeignAutoConfiguration#feignContext() 这里会把FeignClientSpecification  设置进去
  */
 @Configuration
 @EnableFeignClients
